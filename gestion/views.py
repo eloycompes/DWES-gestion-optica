2ab5fcd46e08a446e7ecbbf7e16b6007c16fbdf2
eloycompes <eloycompes@gmail.com>
2026-04-12 18:47:46 +0200

    Implementación de vista y plantilla para el listado de clientes

--- gestion/views.py
+++ gestion/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import Cliente
 
 def home(request):
     return render(request, 'base.html')
+
+def lista_clientes(request):
+    # Traemos todos los clientes de la base de datos
+    clientes = Cliente.objects.all()
+    # Los pasamos a la plantilla en un diccionario llamado 'contexto'
+    return render(request, 'gestion/lista_clientes.html', {'clientes': clientes})
--- optica_project/urls.py
+++ optica_project/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from gestion import views # Importamos las vistas de tu app
+from gestion import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('clientes/', views.lista_clientes, name='lista_clientes'),
 ]
